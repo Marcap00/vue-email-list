b224541299eaa02019dc6f0c7525702c893402b6
Marcap00 <cappellatomarco@yahoo.com>
2024-09-11 14:58:30 +0200

    exercise completed

--- js/script.js
+++ js/script.js
@@ -3,7 +3,26 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-            message: 'Hello Vue 3!',
+            email: '',
+            emails: [],
+            maxLength: 10,
+            urlAPI: 'https://flynn.boolean.careers/exercises/api/random/mail',
         }
+    },
+    methods: {
+        getRandomEmail() {
+            for (let i = 0; i < this.maxLength; i++) {
+                axios.get(this.urlAPI)
+                    .then(response => {
+                        console.log(response.data.response);
+                        this.email = response.data.response;
+                        this.emails.push(this.email);
+                    })
+
+            }
+        },
+    },
+    mounted() {
+        this.getRandomEmail();
     }
 }).mount('#app')
